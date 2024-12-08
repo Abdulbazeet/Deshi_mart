1d0906c333c5007b9105485eda99b51b562d00d3
Abdulbazeet <abdulbazeetbalogun4@gmail.com>
2024-12-08 10:46:41 +0100

    new commit

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -6,4 +6,46 @@ class Global_Variables {
   static const main_color = Color(0xfff53B175);
 
   // static const primary_color = Color(0xFFF53B175);
+  final List<Map<String, String>> categoryProduct = [
+    {
+      'name': "Fresh Fruits & Vegetables",
+      "image": "assets/images/vegetable.png"
+    },
+    {
+      'name': "Cooking Oils",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Meat & Fish",
+      "image": "assets/images/cooking_oil.png",
+    },
+    {
+      'name': "Bakery & Pastries",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Diary & Eggs",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Beverages",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Frozen foods",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Spices & Seasoning",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Personal Care",
+      "image": "assets/images/vegetable.png",
+    },
+    {
+      'name': "Household Supplies",
+      "image": "assets/images/vegetable.png",
+    },
+  ];
 }
